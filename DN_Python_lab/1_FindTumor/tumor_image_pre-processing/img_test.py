edac8c4ab8fb20423cfda1269052c3875b0017f3
LeeDongN <ymj07183@naver.com>
2020-01-30 17:59:15 +0900

    find

--- DN_Python_lab/1_FindTumor/tumor_image_pre-processing/img_test.py
+++ DN_Python_lab/1_FindTumor/tumor_image_pre-processing/img_test.py
@@ -2,10 +2,10 @@ import cv2 as cv
 
 img_test_color = cv.imread(r"C:\program1\image_raw\01-0001.png", cv.IMREAD_COLOR)
 img_test_gray = cv.cvtColor(img_test_color, cv.COLOR_BGR2GRAY)
-resize_img = cv.resize(img_test_gray,(1000, 1000))
-resize_img2 = cv.resize(img_test_color, (500,500))
+resize_img = cv.resize(img_test_gray,(500, 500))
+resize_img2 = cv.resize(img_test_color, (400,400))
 
 
 
-cv.imshow("Show2", resize_img2)
+cv.imshow("Show2", resize_img)
 cv.waitKey(0)
