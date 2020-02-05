b29e62d1b2617bcdf52f75caa587ac1e334e2e89
LeeDongN <ymj07183@naver.com>
2020-02-05 17:49:00 +0900

    find

--- DN_Python_lab/1_FindTumor/tumor_pre-processing/tumor_pre-processing.py
+++ DN_Python_lab/1_FindTumor/tumor_pre-processing/tumor_pre-processing.py
@@ -12,8 +12,36 @@ tif = glob.glob(path_image + '/*.tif')
 png = glob.glob(path_image + '/*.png')
 bmp = glob.glob(path_image + '/*.bmp')
 
-SAVE_PATH = 'C:\program1\imgae_raw_modified\\'
-def load_img(DATA_KIND, SAVE_PATH):
+#img 원하는 저장 공간
+SAVE_PATH_img = 'C:\program1\imgae_raw_modified\\'
+
+
+#mask 원하는 저장 공간
+SAVE_PATH_mask = 'C:\program1\image_mask_modified\\'
+
+#원하는 경로 지정 변수
+path_mask = r'C:\program1\image_mask\\'
+
+tif_mask = glob.glob(path_mask + '/*.tif')
+png_mask = glob.glob(path_mask + '/*.png')
+bmp_mask = glob.glob(path_mask + '/*.bmp')
+
+
+
+def mask_cutting(x, y, w, h, SAVE_PATH_mask, DATA_KIND):
+    
+    for a in DATA_KIND:
+        
+        mask_gray = cv2.imread(a, cv2.IMREAD_GRAYSCALE)
+        basename_m = os.path.basename(a)
+        
+        mask_trim = mask_gray[y:y+h, x:x+w]
+        cv2.imwrite(SAVE_PATH_mask + basename_m, mask_trim)
+        print(basename_m)
+        
+        
+
+def img_cutting(DATA_KIND, SAVE_PATH):
     
 
     for a in DATA_KIND:
@@ -29,7 +57,6 @@ def load_img(DATA_KIND, SAVE_PATH):
 
 
         contours, _ = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        total = 0
 
 
         contours_xy = np.array(contours)
@@ -65,12 +92,18 @@ def load_img(DATA_KIND, SAVE_PATH):
         cv2.imwrite(SAVE_PATH + basename, img_trim)
         print(basename)
 
-start = time.time()
-load_img(bmp, SAVE_PATH)
-load_img(png, SAVE_PATH)    
-end = time.time()
+        mask_cutting(x, y, w, h, SAVE_PATH_mask, tif_mask)
 
-print(round(end - time, 2))       
+
+
+
+
+startTime = time.time()
+img_cutting(bmp, SAVE_PATH_img)
+img_cutting(png, SAVE_PATH_img)    
+endTime = time.time()
+
+print(round(endTime - startTime, 2))       
         
         
         
